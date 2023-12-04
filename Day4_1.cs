c75b98a4124390449811d8e5dc40d395a246ce01
pandakraut <75352338+pandakraut@users.noreply.github.com>
2023-12-04 11:02:37 -0500

    Add files via upload

---
+++ Day4_1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2023
+{
+    class day4_1
+    {
+        static void Main(string[] args)
+        {
+
+            int curTotal = 0;
+            int total = 0;
+            int id = 0;
+            HashSet<string> winners = new HashSet<string>();
+            foreach (string line in File.ReadLines("../../../inputDay4.txt"))
+            {
+                winners.Clear();
+                String[] splitId = line.Split(':');
+                //id = Convert.ToInt32(splitId[0].Split(' ')[1]);
+                string[] splitSide = splitId[1].Split("|");
+                foreach (string value in splitSide[0].Split(" "))
+                {
+                    if (value != "" && !winners.Contains(value))
+                    {
+                        winners.Add(value);
+                    }
+                }
+
+                curTotal = 0;
+                foreach (string heldValue in splitSide[1].Split(" "))
+                {
+                    if (winners.Contains(heldValue))
+                    {
+                        if (curTotal == 0)
+                        {
+                            curTotal = 1;
+                        }
+                        else
+                        {
+                            curTotal *= 2;
+                        }
+                    }
+                }
+                total += curTotal;
+            }
+            Console.WriteLine("output: " + total);
+        }
+    }
+}
---
+++ Day4_2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml.Schema;
+
+namespace AdventOfCode2023
+{
+    class day4_2
+    {        
+        public static Dictionary<int, int> processLine = new Dictionary<int, int>();
+        public static HashSet<string> winners = new HashSet<string>();
+        public static Dictionary<int, int> winLog = new Dictionary<int, int>();
+        public static int total = 0;
+        static void Main(string[] args)
+        {      
+            int id = 0;
+           
+            foreach (string line in File.ReadLines("../../../inputDay4.txt"))
+            {
+                String[] splitId = line.Split(':');
+                id = Convert.ToInt32(splitId[0].Replace("Card", "").Trim());
+ 
+                if (processLine.ContainsKey(id))
+                {
+                    processLine[id]++;
+                    total++;
+                    for (int i = 0; i < processLine[id]; i++)
+                    {
+                        if (winLog.ContainsKey(id))
+                        {
+                            addLines(id, winLog[id]);                            
+                        }                    
+                        else
+                        {
+                            checkWinners(splitId, id);
+                        }
+                    }
+                }
+                else
+                {
+                    processLine.Add(id, 1);
+                    total++;
+                    checkWinners(splitId, id);                    
+                }                
+            }
+            Console.WriteLine("output: " + total);
+        }
+
+        public static void addLines(int id, int curTotal)
+        {
+            int nextId = id;
+            nextId++;
+            
+            for (int newId = nextId; newId <= id + curTotal; newId++)
+            {
+                if (processLine.ContainsKey(newId))
+                {
+                    processLine[newId]++;
+                }
+                else
+                {
+                    processLine.Add(newId, 1);
+                }
+                total++;
+            }
+        }
+
+        public static void checkWinners(string[] splitId, int id)
+        {
+            int curTotal = 0;
+            winners.Clear();
+            string[] splitSide = splitId[1].Split("|");
+            foreach (string value in splitSide[0].Split(" "))
+            {
+                if (value != "" && !winners.Contains(value))
+                {
+                    winners.Add(value);
+                }
+            }
+            
+            foreach (string heldValue in splitSide[1].Split(" "))
+            {
+                if (winners.Contains(heldValue))
+                {
+                    curTotal++;
+                }
+            }
+            winLog.Add(id, curTotal);            
+            addLines(id, curTotal);
+            
+        }
+    }
+}
