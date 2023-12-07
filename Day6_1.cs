c06bbf0ecc428b8cb5b8a01681b6ac3eb0b2ddd8
pandakraut <75352338+pandakraut@users.noreply.github.com>
2023-12-06 19:08:14 -0500

    Add files via upload

---
+++ Day6_1.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.IO;
+//using System.Collections.Generic;
+
+//namespace AdventOfCode2023
+//{
+//    class day6_1
+//    {
+//        static void Main(string[] args)
+//        {
+            
+//            int total = 1;
+//            string id = "";
+//            List<int> times = new List<int>();
+//            List<int> distances = new List<int>();
+//            foreach (string line in File.ReadLines("../../../inputDay6.txt"))
+//            {                
+//                String[] splitId = line.Split(':'); 
+//                id = splitId[0];
+//                string[] splitRace = splitId[1].Split(" ");
+//                foreach (string value in splitRace)
+//                {
+//                    if (value.Trim() != "")
+//                    {
+//                        if (id == "Time")
+//                        {
+//                            times.Add(Convert.ToInt32(value));
+//                        }
+//                        else
+//                        {
+//                            distances.Add(Convert.ToInt32(value));
+//                        }                        
+//                    }
+//                }                                               
+//            }
+//            for (int i = 0; i < times.Count; i++)
+//            {
+//                total *= checkWins(i, times[i], distances[i]);
+//            }
+//            Console.WriteLine("output: " + total);
+//        }
+
+//        public static int checkWins(int index, int time, int distance)
+//        {
+//            int wins = 0;
+//            for (int i = 1; i < time; i++)
+//            {
+//                if (((time - i) * i) > distance)
+//                {
+//                    wins++;
+//                }
+//            }
+//            return wins;
+//        }
+//    }
+//}
---
+++ Day6_2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2023
+{
+    class day6_2
+    {
+        static void Main(string[] args)
+        {
+            
+            long total = 1;
+            string id = "";
+            List<long> times = new List<long>();
+            List<long> distances = new List<long>();
+            foreach (string line in File.ReadLines("../../../inputDay6.txt"))
+            {
+                String[] splitId = line.Replace(" ", "").Split(':');
+                id = splitId[0];
+                if (splitId[1].Trim() != "")
+                {
+                    if (id == "Time")
+                    {
+                        times.Add((long)Convert.ToDecimal(splitId[1]));
+                    }
+                    else
+                    {
+                        distances.Add((long)Convert.ToDecimal(splitId[1]));
+                    }
+                }                                                       
+            }
+            for (int i = 0; i < times.Count; i++)
+            {
+                total *= checkWins(i, times[i], distances[i]);
+            }
+            Console.WriteLine("output: " + total);
+        }
+
+        public static int checkWins(int index, long time, long distance)
+        {
+            int wins = 0;
+            for (long i = 1; i < time; i++)
+            {
+                if (((time - i) * i) > distance)
+                {
+                    wins++;
+                }
+            }
+            return wins;
+        }
+    }
+}
